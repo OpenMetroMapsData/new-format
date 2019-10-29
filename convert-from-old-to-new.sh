6551819b1d2446dfb9a1644db3b0555cf4c35054
Sebastian Kürten <sebastian@topobyte.de>
2019-10-29 19:26:55 +0100

    When converting existing files, create variants with straight segments

--- convert-from-old-to-new.sh
+++ convert-from-old-to-new.sh
@@ -4,6 +4,8 @@ OMM_CLI=~/"github/OpenMetroMaps/OpenMetroMaps/java/scripts/openmetromaps-cli"
 OMM_DATA=~/"github/OpenMetroMapsData"
 OMM_NEW=~/"github/OpenMetroMapsData/new-format"
 
+set -e
+
 # Berlin
 mkdir -p "$OMM_NEW/berlin"
 "$OMM_CLI" new-format create \
@@ -13,6 +15,14 @@ mkdir -p "$OMM_NEW/berlin"
 "$OMM_CLI" new-format create \
     --input "$OMM_DATA/berlin/schematic.omm" \
     --output "$OMM_NEW/berlin/schematic.xml"
+
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/berlin/geographic.omm" \
+    --output "$OMM_NEW/berlin/geographic-straight.xml"
+
+"$OMM_CLI" new-format create -segment-mode straight \
+    --input "$OMM_DATA/berlin/schematic.omm" \
+    --output "$OMM_NEW/berlin/schematic-straight.xml"
 
 # VBB
 mkdir -p "$OMM_NEW/vbb-regio"
@@ -24,6 +34,14 @@ mkdir -p "$OMM_NEW/vbb-regio"
     --input "$OMM_DATA/vbb-regio/schematic.omm" \
     --output "$OMM_NEW/vbb-regio/schematic.xml"
 
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/vbb-regio/geographic.omm" \
+    --output "$OMM_NEW/vbb-regio/geographic-straight.xml"
+
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/vbb-regio/schematic.omm" \
+    --output "$OMM_NEW/vbb-regio/schematic-straight.xml"
+
 # Vienna
 mkdir -p "$OMM_NEW/vienna"
 "$OMM_CLI" new-format create \
@@ -33,6 +51,14 @@ mkdir -p "$OMM_NEW/vienna"
 "$OMM_CLI" new-format create \
     --input "$OMM_DATA/vienna/schematic.omm" \
     --output "$OMM_NEW/vienna/schematic.xml"
+
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/vienna/geographic.omm" \
+    --output "$OMM_NEW/vienna/geographic-straight.xml"
+
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/vienna/schematic.omm" \
+    --output "$OMM_NEW/vienna/schematic-straight.xml"
 
 # Stuttgart
 mkdir -p "$OMM_NEW/stuttgart"
@@ -44,6 +70,14 @@ mkdir -p "$OMM_NEW/stuttgart"
     --input "$OMM_DATA/stuttgart/sbahn-geographic.omm" \
     --output "$OMM_NEW/stuttgart/geographic.xml"
 
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/stuttgart/sbahn-schematic.omm" \
+    --output "$OMM_NEW/stuttgart/schematic-straight.xml"
+
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/stuttgart/sbahn-geographic.omm" \
+    --output "$OMM_NEW/stuttgart/geographic-straight.xml"
+
 # BART
 mkdir -p "$OMM_NEW/bart"
 "$OMM_CLI" new-format create \
@@ -54,14 +88,30 @@ mkdir -p "$OMM_NEW/bart"
     --input "$OMM_DATA/bart/geographic.omm" \
     --output "$OMM_NEW/bart/geographic.xml"
 
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/bart/schematic.omm" \
+    --output "$OMM_NEW/bart/schematic-straight.xml"
+
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/bart/geographic.omm" \
+    --output "$OMM_NEW/bart/geographic-straight.xml"
+
 # Managua
 mkdir -p "$OMM_NEW/managua"
 "$OMM_CLI" new-format create \
     --input "$OMM_DATA/managua/geographic.omm" \
     --output "$OMM_NEW/managua/geographic.xml"
 
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/managua/geographic.omm" \
+    --output "$OMM_NEW/managua/geographic-straight.xml"
+
 # Gueret
 mkdir -p "$OMM_NEW/gueret"
 "$OMM_CLI" new-format create \
     --input "$OMM_DATA/gueret/geographic.omm" \
     --output "$OMM_NEW/gueret/geographic.xml"
+
+"$OMM_CLI" new-format create --segment-mode straight \
+    --input "$OMM_DATA/gueret/geographic.omm" \
+    --output "$OMM_NEW/gueret/geographic-straight.xml"
